dfb5789ff064ae84c4fd909c9cf4cab85db39906
kyloReneo <saeedderayat2000@gmail.com>
2024-04-20 21:42:57 +0330

    fixing gin package version

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/gin-gonic/gin"
+
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080
+}
