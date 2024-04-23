5e029612aebe0964213563c8205a8481b665e6fb
kyloReneo <saeedderayat2000@gmail.com>
2024-04-23 11:10:32 +0330

    assets pkg

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,14 @@ import (
 	"github.com/kyloReneo/go-blog/pkg/config"
 	"github.com/kyloReneo/go-blog/pkg/html"
 	"github.com/kyloReneo/go-blog/pkg/routing"
+	"github.com/kyloReneo/go-blog/pkg/static"
 
 )
 
 func Serve() {
 	config.Set()
 	routing.Init()
+	static.LoadStatic(routing.GetRouter())
 	html.LoadHTML(routing.GetRouter())
 	routing.RegisterRoutes()
 	routing.Serve()
