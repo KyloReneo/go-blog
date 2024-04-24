6f2b29eda47cbb300a1c9e6018a9c70bed9da930
kyloReneo <saeedderayat2000@gmail.com>
2024-04-24 10:18:39 +0330

    seed init

---
+++ cmd/seed.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+)
+
+func init() {
+	rootCmd.AddCommand(seedCmd)
+}
+
+var seedCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "Database seeder",
+	Long:  `Creates Random seed for database table context.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		Seed()
+	},
+}
+
+func Seed() {
+
+}
