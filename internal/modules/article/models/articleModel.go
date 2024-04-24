3fc00772f61f190e4eea62baa3e2e450c3392db9
kyloReneo <saeedderayat2000@gmail.com>
2024-04-24 09:45:44 +0330

    create models

---
+++ internal/modules/article/models/articleModel.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/kyloReneo/go-blog/internal/modules/user/models"
+)
+
+type Article struct {
+	gorm.Model
+	Title   string `gorm:"varchar:191"`
+	Content string `gorm:"text"`
+	UserID  uint
+	User    models.User
+}
