7dfb8b376ebbc555bfb663bd08e13c613fc55684
kyloReneo <saeedderayat2000@gmail.com>
2024-04-24 11:48:41 +0330

    article repository controller

--- internal/modules/article/repositories/articleRepository.go
+++ internal/modules/article/repositories/articleRepository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"gorm.io/gorm"
 
+	articleModel "github.com/kyloReneo/go-blog/internal/modules/article/models"
 	"github.com/kyloReneo/go-blog/pkg/database"
 
 )
@@ -15,4 +16,12 @@ func New() *ArticleRepository {
 	return &ArticleRepository{
 		DB: database.Connection(),
 	}
+}
+
+func (articleRepository *ArticleRepository) List(limit int) []articleModel.Article {
+	var articles []articleModel.Article
+
+	articleRepository.DB.Limit(limit).Joins("User").Order("rand()").Find(&articles)
+
+	return articles
 }
--- internal/modules/home/controllers/homeController.go
+++ internal/modules/home/controllers/homeController.go
@@ -5,18 +5,23 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	ArticleRepository "github.com/kyloReneo/go-blog/internal/modules/article/repositories"
 	"github.com/kyloReneo/go-blog/pkg/html"
 
 )
 
 // Define a controller type struct and a function that returns a Controller instance
-type Controller struct{}
-
-func New() *Controller {
-	return &Controller{}
+type Controller struct {
+	articleRepository ArticleRepository.ArticleRepositoryInterface
 }
 
-//Create a handler function for returning the gin context
+func New() *Controller {
+	return &Controller{
+		articleRepository: ArticleRepository.New(),
+	}
+}
+
+// Create a handler function for returning the gin context
 func (controller *Controller) Index(ctx *gin.Context) {
 
 	html.Render(ctx, http.StatusOK, "../html/home", gin.H{
