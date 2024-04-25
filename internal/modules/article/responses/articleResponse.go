accb4cf92ffeb991223be301946805502827dc32
kyloReneo <saeedderayat2000@gmail.com>
2024-04-25 11:59:52 +0330

    article response

---
+++ internal/modules/article/responses/articleResponse.go
@@ -0,0 +1,37 @@
+package responses
+
+import (
+	"fmt"
+
+	articleModel "github.com/kyloReneo/go-blog/internal/modules/article/models"
+	userResponse "github.com/kyloReneo/go-blog/internal/modules/user/responses"
+)
+
+// Response structure for the article responses
+type Article struct {
+	ID        uint
+	Image     string
+	Title     string
+	Content   string
+	CreatedAt string
+	User      userResponse.User
+}
+
+// Structure for return bunch of Articles
+type Articles struct {
+	Data []Articles
+}
+
+// Recives the model object and transforms it to response object
+func ToArticle(article articleModel.Article) Article {
+	return Article{
+		ID:      article.ID,
+		Title:   article.Title,
+		Content: article.Content,
+		CreatedAt: fmt.Sprintf("%d/%02d/%02d",
+			article.CreatedAt.Year(),
+			article.CreatedAt.Month(),
+			article.CreatedAt.Day()),
+		Image: "/assets/img/demopic/10.jpg",
+	}
+}
--- internal/modules/user/responses/userResponse.go
+++ internal/modules/user/responses/userResponse.go
@@ -4,24 +4,26 @@ import (
 	"fmt"
 
 	userModel "github.com/kyloReneo/go-blog/internal/modules/user/models"
-
 )
 
+// Response structure for the user responses
 type User struct {
-	ID uint
+	ID    uint
 	Image string
-	Name string
+	Name  string
 	Email string
 }
 
+// Structure for return bunch of users
 type Users struct {
 	Data []User
 }
 
-func ToUser (user userModel.User) User {
+// Recives the model object and transforms it to response object
+func ToUser(user userModel.User) User {
 	return User{
-		ID: user.ID,
-		Name: user.Name,
+		ID:    user.ID,
+		Name:  user.Name,
 		Email: user.Email,
 		Image: fmt.Sprintf("https://ui-avatar.com/api/?name=%s", user.Name),
 	}
