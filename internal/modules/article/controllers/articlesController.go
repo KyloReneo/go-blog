2ef06821664c10e5a870b58a7fd67f24d61379ae
kyloReneo <saeedderayat2000@gmail.com>
2024-04-26 21:35:41 +0330

    error response pages

--- internal/modules/article/controllers/articlesController.go
+++ internal/modules/article/controllers/articlesController.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	ArticleService "github.com/kyloReneo/go-blog/internal/modules/article/services"
-
+	"github.com/kyloReneo/go-blog/pkg/html"
 )
 
 // Define a controller type struct and a function that returns a Controller instance
@@ -27,25 +27,34 @@ func (controller *Controller) Show(ctx *gin.Context) {
 	//Get the article id
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"Error": "Somthing went wrong with converting the id.",
-		}) 
+		html.Render(ctx,
+			http.StatusInternalServerError,
+			"/templates/errors/html/500",
+			gin.H{
+				"title":   "Internal error",
+				"Message": "Converting id error",
+			})
 		return
-	} 
+	}
 
 	//Find the article in the database
 	article, err := controller.articleService.Find(id)
 	//Show error page if the article is not found
 	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"Error": err.Error(),
-		})
+		html.Render(ctx,
+			http.StatusNotFound,
+			"/templates/errors/html/404",
+			gin.H{
+				"title":   "Page not found",
+				"Message": err.Error(),
+			})
 		return
-	} 
+
+	}
 
 	//render the article template
 	ctx.JSON(http.StatusOK, gin.H{
-		"article" : article,
+		"article": article,
 	})
 
 }
