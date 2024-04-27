094fb042111be4ce4ea3839c5b4d81cc5e0d11c6
kyloReneo <saeedderayat2000@gmail.com>
2024-04-27 11:31:22 +0330

    usr validation

--- internal/modules/user/controllers/authController.go
+++ internal/modules/user/controllers/authController.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/kyloReneo/go-blog/internal/modules/user/requests/auth"
 	"github.com/kyloReneo/go-blog/pkg/html"
 
 )
@@ -20,12 +21,24 @@ func New() *Controller {
 // A handler function for GET the "/register" requestes
 func (controller *Controller) Register(ctx *gin.Context) {
 
-	html.Render(ctx, http.StatusOK,"../../user/html/register", gin.H{
-		
-	})
+	html.Render(ctx, http.StatusOK, "../../user/html/register", gin.H{})
 }
 
 // A handler function for POST the "/register" requestes
 func (controller *Controller) HandleRegister(ctx *gin.Context) {
+
+	// Validate the request
+	var request auth.RegisterRequest
+	// This will infer what binder to use depending on the content-type header.
+	if err := ctx.ShouldBind(&request); err != nil {
+		ctx.Redirect(http.StatusFound, "/register")
+		return
+	}
+
+	// Create the user
+
+	// Check if there is any error on the user creation
+
+	// After creating the user redirects to the home page
 
 }
---
+++ internal/modules/user/requests/auth/registerRequest.go
@@ -0,0 +1,8 @@
+package auth
+
+// Binding from html form
+type RegisterRequest struct {
+	Name     string `form:"name" binding:"required, min=3, max=100"`
+	Email    string `form:"email" binding:"required, email, min=3, max=100"`
+	Password string `form:"password"  binding:"required, min=8, max=32"`
+}
