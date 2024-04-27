fd7aea137a206b4ffe8ed0bad34dbba03986e7d6
kyloReneo <saeedderayat2000@gmail.com>
2024-04-27 11:12:10 +0330

    register tmpl

--- internal/modules/user/controllers/authController.go
+++ internal/modules/user/controllers/authController.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"github.com/kyloReneo/go-blog/pkg/html"
+
 )
 
 // Define a controller type struct and a function that returns a Controller instance
@@ -15,6 +20,9 @@ func New() *Controller {
 // A handler function for GET the "/register" requestes
 func (controller *Controller) Register(ctx *gin.Context) {
 
+	html.Render(ctx, http.StatusOK,"../../user/html/register", gin.H{
+		
+	})
 }
 
 // A handler function for POST the "/register" requestes
