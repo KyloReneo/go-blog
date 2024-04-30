91b2b79c2e4d8a2b9ec61dc74f1d1dff434bf402
kyloReneo <saeedderayat2000@gmail.com>
2024-04-30 19:25:15 +0330

    old data finished

--- internal/providers/view/html.go
+++ internal/providers/view/html.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/kyloReneo/go-blog/pkg/converters"
 	"github.com/kyloReneo/go-blog/pkg/sessions"
-
 )
 
 func WithGlobalData(ctx *gin.Context, data gin.H) gin.H {
 	data["APP_NAME"] = viper.Get("App.Name")
 	data["ERRORS"] = converters.StringToMap(sessions.Flash(ctx, "errors"))
+	data["OLD"] = converters.StringToUrlValues(sessions.Flash(ctx, "old"))
 	return data
 }
