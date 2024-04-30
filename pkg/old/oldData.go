43d8569353d5c3b8564dfd66fb1523d9e872bd71
kyloReneo <saeedderayat2000@gmail.com>
2024-04-30 19:09:26 +0330

    old data

--- pkg/old/oldData.go
+++ pkg/old/oldData.go
@@ -4,11 +4,15 @@ import "github.com/gin-gonic/gin"
 
 var oldList = make(map[string][]string)
 
-func init() {
+func Init() {
 	oldList = map[string][]string{}
 }
 
-func set(ctx *gin.Context) {
+func Set(ctx *gin.Context) {
 	ctx.Request.ParseForm()
 	oldList = ctx.Request.PostForm
+}
+
+func Get() map[string][]string {
+	return oldList
 }
